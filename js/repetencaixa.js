0747ec11a420582ab1092b43acc2a31182cfddee
nadfelipe <nadfelipe.s@gmail.com>
2023-09-27 17:22:43 -0300

    Adiciona canva home

--- js/repetencaixa.js
+++ js/repetencaixa.js
@@ -1,143 +1,58 @@
-let palavras = [];
-let tilesX = 20;
-let tilesY = 1;
+let rects = [];
+let tilesX = 10;
+let tilesY = 15;
 let tileW, tileH, font;
-let larguraLinhaRect = '2';
-const palavraWidths = {};
-const wordImages = {};
-let frase = "R E P E T E ⮕ E ⮕ E N C A II X A";
-
-function preload() {
-  font = loadFont("../assets/Sk-Modernist-Regular.otf");
-}
+let colorBg = "#FF6689";
+let colorStroke = "#FCD422";
+let colorRect = "#2ec2b3";
+let colorRectAlt = "#332783";
 
 function setup() {
-  createCanvas(1440, 40, document.getElementById('canvas-footer'));
+  createCanvas(1000, 1000, document.getElementById("canvas-apresentacao"));
+  stroke(color(colorStroke));
   tileW = int(width / tilesX);
   tileH = int(height / tilesY);
-  drawGrid();
-  displayFrase(frase);
-}
-
-function draw() {
-    
-  background(`#332783`);
-  for (let i = 0; i < palavras.length; i++) {
-    palavras[i].display();
-    // movimentaPalavra(palavras[i]);
-  }
-}
-
-function movimentaPalavra(palavra) {
-    palavra.direita();
-}
-
-function drawGrid() {
   for (let y = 0; y < tilesY; y++) {
     for (let x = 0; x < tilesX; x++) {
-      fill(0, 255, 0);
-      strokeWeight(2);
+      fill(color(colorBg));
+      strokeWeight(3);
       rectMode(CORNER);
       rect(x * tileW, y * tileH, tileW, tileH);
     }
   }
 }
 
-class Palavra {
-    constructor(x, y, w, h, texto) {
-        this.x = x;
-        this.y = y;
-        this.texto = texto;
-        this.w = w;
-        this.h = h;
-        this.wordWidth = 1;
-        this.pg = null;
-        this.corTexto = `#FFFCEE`;
-        this.createPG();
-    }
-  
-    createPG() {
-        if (this.w < 1){
-            this.w = 1
-            this.wordWidth = 1
-        }
-
-        this.pg = createGraphics(tileW * 1, tileH * 1);
-        this.pg.fill(`#FF3333`);
-        this.pg.stroke(`#FF6689`);
-        this.pg.strokeWeight(larguraLinhaRect);
-        this.pg.rect(0, 0, tileW * 1, tileH * 1);
-        this.pg.noStroke();
-        this.pg.textFont(font);
-        this.pg.fill(this.corTexto);
-        this.pg.textSize(tileH);
-        this.pg.textAlign(LEFT, CENTER);
-        this.pg.text(this.texto, 23, tileH / 2 - 6);
-    }
-  
-    display() {
-        let col = this.x % tilesX;
-        let row = this.y % tilesY;
-        let imgX = col * tileW;
-        let imgY = row * tileH;
-
-        if (this.w < 1)
-        this.w = 1
-
-        image(this.pg, imgX, imgY, tileW * 1, tileH * this.h);
-    }
-  
-    direita() {
-        this.x = this.x + 0.1;
-
-        if (this.x >= tilesX) this.x = 0;
+function draw() {
+  fill(color(colorRectAlt));
+  rectMode(CENTER);
+  rect(mouseX, mouseY, tileW, tileH)
+  for (let i = 0; i < rects.length; i++){
+    rects[i].display();
+    if(mouseIsPressed){
+      rects[i].movimentar()
     }
   }
-  
-  function calculateWordWidths() {
-    const palavrasFrase = frase.split(" ");
-    for (let i = 0; i < palavrasFrase.length; i++) {
-      const palavra = palavrasFrase[i].toUpperCase();
-      palavraWidths[palavra] = Math.round(int(textWidth(palavra)) / 10);
-    }
-  }
+}
 
-  function displayFrase(frase) {
-    let col = 0;
-    let row = 0;
-    calculateWordWidths();
-    const palavrasFrase = frase.split(" ");
-  
-    for (let i = 0; i < palavrasFrase.length; i++) {
-      const larguraPalavra = palavraWidths[palavrasFrase[i].toUpperCase()];
-  
-      if (col + larguraPalavra <= tilesX) {
-        palavras.push(
-          new Palavra(
-            col,
-            row,
-            larguraPalavra,
-            1,
-            `${palavrasFrase[i].toUpperCase()}`
-          )
-        );
-        col += larguraPalavra;
-      } else {
-        col = 0;
-        row++;
-        if (row >= tilesY) {
-          break;
-        }
-        palavras.push(
-          new Palavra(
-            col,
-            row,
-            larguraPalavra,
-            1,
-            `${palavrasFrase[i].toUpperCase()}`
-          )
-        );
-        col += larguraPalavra;
-      }
-    }
+function mouseClicked() {
+  rects.push(new Quadrado(mouseX, mouseY, tileW * random(1, 1.2), tileH * random(1, 1.2)));
+}
+
+class Quadrado {
+  constructor(x, y, w, h) {
+      this.x = x;
+      this.y = y;
+      this.w = w;
+      this.h = h;
   }
+  
+  display() {
+    fill(color(colorRect));
+    rect(this.x, this.y, this.w, this.h);
+  }
+  
+  movimentar() {
+    this.x = this.x + int(random(-1, 1) * 10);
+    this.y = this.y + int(random(-1, 1) * 10);
+  }
+}
