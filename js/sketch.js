c3f9432147a50265720a02ad3b1d143e231061ba
nadfelipe <nadfelipe.s@gmail.com>
2023-09-27 04:54:21 -0300

    Estiliza gerador de layout

--- js/sketch.js
+++ js/sketch.js
@@ -2,10 +2,19 @@ let palavras = [];
 let tilesX = 10;
 let tilesY = 10;
 let tileW, tileH, font;
-let larguraLinhaRect = '2';
+let larguraLinhaRect = "2";
 let modoMovimento;
 let movimentoEscolhido;
-const movimentos = ["direita", "esquerda", "subir", "descer", "sobeEsq", "desceEsq", "sobeDir", "desceDir"];
+const movimentos = [
+  "direita",
+  "esquerda",
+  "subir",
+  "descer",
+  "sobeEsq",
+  "desceEsq",
+  "sobeDir",
+  "desceDir",
+];
 const palavraWidths = {};
 const wordImages = {};
 let frase = "Um pequeno jabuti xereta viu dez cegonhas felizes.";
@@ -15,10 +24,10 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1080, 1080, document.getElementById('gerador-layout'));
+  createCanvas(1080, 1080, document.getElementById("gerador-layout"));
   tileW = int(width / tilesX);
   tileH = int(height / tilesY);
-  background(`${document.getElementById('cor-bg').value}`);
+  background(`${document.getElementById("cor-bg").value}`);
   // drawGrid();
   displayFrase(frase);
 }
@@ -32,25 +41,24 @@ function draw() {
   setModoMovimento();
 }
 
-function setMovimento(palavra){
+function setMovimento(palavra) {
   switch (modoMovimento) {
     case "treme":
       movimentaPalavra(palavra, true);
       break;
     case "direcao":
-      movimentaPalavra(palavra)
+      movimentaPalavra(palavra);
       break;
     default:
       palavra.subir;
-      palavra.descer
+      palavra.descer;
       break;
   }
 }
 
 function movimentaPalavra(palavra, aleatorio) {
-  if (aleatorio)
-    movimentoEscolhido = random(movimentos);
-  
+  if (aleatorio) movimentoEscolhido = random(movimentos);
+
   switch (movimentoEscolhido) {
     case "direita":
       palavra.direita();
@@ -85,60 +93,95 @@ function movimentaPalavra(palavra, aleatorio) {
 
 function setModoMovimento() {
   let radioButtons = document.getElementsByName("modoMovimento");
-  
+
   for (let i = 0; i < radioButtons.length; i++) {
-      if (radioButtons[i].checked) {
-          modoMovimento = radioButtons[i].value;
-          break;
-      }
+    if (radioButtons[i].checked) {
+      modoMovimento = radioButtons[i].value;
+      break;
+    }
   }
 
-  if (modoMovimento == 'direcao') {
+  if (modoMovimento == "direcao") {
     let radioButtonsDirecao = document.getElementsByName("direcao");
 
     for (let i = 0; i < radioButtonsDirecao.length; i++) {
       if (radioButtonsDirecao[i].checked) {
-          movimentoEscolhido = radioButtonsDirecao[i].value;
-          break;
+        movimentoEscolhido = radioButtonsDirecao[i].value;
+        break;
       }
+    }
   }
-  }
-  
 }
 
 function atualizarConteudo() {
-  background(`${document.getElementById('cor-bg').value}`);
+  background(`${document.getElementById("cor-bg").value}`);
   limparPalavras();
   frase = `${document.querySelector("#frase").value}`;
   displayFrase(frase);
+  verificaCheckbox();
 }
 
-function limparPalavras(){
+function limparPalavras() {
   for (let i = 0; i < palavras.length; i++) {
     palavras[i].pg.remove();
   }
   palavras = [];
 }
 
+function resetInputs() {
+  resetRadio("modoMovimento");
+  resetRadio("direcao");
+  document.getElementById("cor-bg").value = "#332783";
+  document.getElementById("cor-texto").value = "#FFFCEE";
+  document.getElementById("cor-rect").value = "#FF3333";
+  document.getElementById("cor-stroke").value = "#FF6689";
+  document.getElementById("frase").value = "Um pequeno jabuti xereta viu dez cegonhas felizes.";
+}
+
+function resetRadio(name) {
+  let radioInputs = document.querySelectorAll(`input[type="radio"][name="${name}"]`);
+
+  radioInputs[0].checked = true;
+
+  radioInputs.forEach(function(input, index) {
+    let label = document.querySelector('label[for="' + input.id + '"]');
+    if (index === 0) {
+      input.checked = true;
+      label.classList.remove('radio-unchecked-style');
+    } else {
+      input.checked = false;
+      label.classList.add('radio-unchecked-style');
+    }
+  });
+}
+
 function resetCanvas() {
+  resetInputs();
   limparPalavras();
-  clear();
   frase = "Um pequeno jabuti xereta viu dez cegonhas felizes.";
   displayFrase(frase);
-  background(`${document.getElementById('cor-bg').value}`);
+  background(`${document.getElementById("cor-bg").value}`);
 }
 
 function salvarLayout() {
-  save('layout.png');
+  save("layout.png");
 }
 
 function drawGrid() {
   for (let y = 0; y < tilesY; y++) {
     for (let x = 0; x < tilesX; x++) {
-      fill(0, 255, 0);
+      stroke(`${document.getElementById("cor-stroke").value}`);
       strokeWeight(2);
       rectMode(CORNER);
+      fill(`${document.getElementById("cor-bg").value}`);
       rect(x * tileW, y * tileH, tileW, tileH);
     }
+  }
+}
+
+function verificaCheckbox() {
+  var checkbox = document.getElementById("grid");
+  if (checkbox.checked) {
+    drawGrid();
   }
 }
